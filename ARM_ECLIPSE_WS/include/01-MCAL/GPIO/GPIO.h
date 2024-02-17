67f3c60ae1b6a1e240f3a67d4a598e5c37d8fc30
Karim Salah <karimsalah917@gmail.com>
2024-02-18 01:54:14 +0200

    MCAL->GPIO ..Update Progress..

--- ARM_ECLIPSE_WS/include/01-MCAL/GPIO/GPIO.h
+++ ARM_ECLIPSE_WS/include/01-MCAL/GPIO/GPIO.h
@@ -64,11 +64,23 @@ typedef enum
 {
     GPIO_MODE_ANALOG        = 0x00000003,
 
-    GPIO_MODE_INPUT_FLOAT   = 0x00000000,
-    GPIO_MODE_INPUT_PU      = 0x00000000,
-    GPIO_MODE_INPUT_PD      = 0x00000000,
+    GPIO_MODE_INPUT_FL      = 0x00000000,
+    GPIO_MODE_INPUT_PU      = 0x00000008,
+    GPIO_MODE_INPUT_PD      = 0x00000010,
 
-    
+    GPIO_MODE_OUT_PP_FL     = 0x00000001,   
+    GPIO_MODE_OUT_PP_PU     = 0x00000009,
+    GPIO_MODE_OUT_PP_PD     = 0x00000011,
+    GPIO_MODE_OUT_OD_FL     = 0x00000005,
+    GPIO_MODE_OUT_OD_PD     = 0x0000000D,
+    GPIO_MODE_OUT_OD_PD     = 0x00000015,
+
+    GPIO_MODE_AF_PP_FL      = 0x00000002,   
+    GPIO_MODE_AF_PP_PU      = 0x0000000A,
+    GPIO_MODE_AF_PP_PD      = 0x00000012,
+    GPIO_MODE_AF_OD_FL      = 0x00000006,
+    GPIO_MODE_AF_OD_PD      = 0x0000000E,
+    GPIO_MODE_AF_OD_PD      = 0x00000016
 
 }GPIO_MODE_t;
 
@@ -94,9 +106,42 @@ typedef struct
     GPIO_SPEED_t   SPEED;
 }
 GPIO_CONFIG_t;
+
+/**
+ * @brief Created Data Type for Pin value at output mode of any pin
+*/
+typedef enum 
+{
+    PIN_STATE_LOW  =0,
+    PIN_STATE_HIGH =1
+}
+PIN_STATE_t;
 /**********************************   API's ************************************************/
-GPIO_Error_t GPIO_INIT_PIN(GPIO_CONFIG_t * COPY_CONFIGURATION);
 
-GPIO_Error_t GPIO_SetPinValue(uint32 GPIO_PORT,uint32 GPIO_PIN,uint32 PIN_STATE);
+/**
+ * @brief Initializes a GPIO pin with the specified configuration.
+ *
+ * @param COPY_CONFIGURATION Pointer to the GPIO configuration structure.
+ * @return GPIO_Error_t Returns an error code indicating the success or failure of the operation.
+ */
+GPIO_Error_t GPIO_INIT_PIN(GPIO_CONFIG_t * ADD_CONFIGURATION);
 
-GPIO_Error_t GPIO_GetPinValue(uint32 GPIO_PORT,uint32 GPIO_PIN,uint8 * Copy_PinValue);
+/**
+ * @brief Sets the value of a GPIO pin to the specified state.
+ *
+ * @param COPY_GPIO_PORT The GPIO port to which the pin belongs.
+ * @param COPY_GPIO_PIN The GPIO pin number to set the value for.
+ * @param COPT_PIN_STATE The state to set the GPIO pin to (e.g., HIGH or LOW).
+ * @return GPIO_Error_t Returns an error code indicating the success or failure of the operation.
+ */
+GPIO_Error_t GPIO_SetPinValue(GPIO_PORT_t  COPY_GPIO_PORT, GPIO_PIN_t COPY_GPIO_PIN, PIN_STATE_t COPT_PIN_STATE);
+
+/**
+ * @brief Retrieves the current value of a GPIO pin.
+ *
+ * @param GPIO_PORT The GPIO port to which the pin belongs.
+ * @param GPIO_PIN The GPIO pin number to get the value for.
+ * @param Copy_PinValue Pointer to store the retrieved pin value.
+ * @return GPIO_Error_t Returns an error code indicating the success or failure of the operation.
+ */
+GPIO_Error_t GPIO_GetPinValue(GPIO_PORT_t GPIO_PORT,GPIO_PIN_t GPIO_PIN,uint8 * Copy_PinValue);
--- ARM_ECLIPSE_WS/src/01-MCAL/GPIO/GPIO.c
+++ ARM_ECLIPSE_WS/src/01-MCAL/GPIO/GPIO.c
@@ -11,7 +11,9 @@
 /**********************************  Includes **********************************************/
 #include "../../../include/01-MCAL/GPIO/GPIO.h"
 /********************************** Definitions ********************************************/
-
+#define GPIOA_BASE_ADDRESS                      0x40020000                     
+#define GPIOB_BASE_ADDRESS                      0x40020400
+#define GPIOC_BASE_ADDRESS                      0x40020800
 /**********************************   Types ************************************************/
 
 typedef struct
@@ -28,7 +30,31 @@ typedef struct
     volatile uint32 AFRH;
 }GPIO_t;
 /**********************************  Variables *********************************************/
-
+static GPIO_t * const GPIO[3] =
+{
+    (GPIO_t*)GPIOA_BASE_ADDRESS,
+    (GPIO_t*)GPIOB_BASE_ADDRESS,
+    (GPIO_t*)GPIOC_BASE_ADDRESS
+};
 /********************************** Static Functions ***************************************/
 
 /********************************** Implementation *****************************************/
+
+/**
+ * @brief Initializes a GPIO pin with the specified configuration.
+ *
+ * @param COPY_CONFIGURATION Pointer to the GPIO configuration structure.
+ * @return GPIO_Error_t Returns an error code indicating the success or failure of the operation.
+ */
+GPIO_Error_t GPIO_INIT_PIN(GPIO_CONFIG_t * ADD_CONFIGURATION)
+{
+    uint32 LocalTempMODER =
+    GPIO[ADD_CONFIGURATION->PORT]-> MODER &=;
+    GPIO[ADD_CONFIGURATION->PORT]-> OSPEEDR=;
+    GPIO[ADD_CONFIGURATION->PORT]->OTYPER;
+    GPIO[ADD_CONFIGURATION->PORT]->PUPDR;
+}
+
+GPIO_Error_t GPIO_SetPinValue(GPIO_PORT_t  COPY_GPIO_PORT, GPIO_PIN_t COPY_GPIO_PIN, PIN_STATE_t COPT_PIN_STATE);
+
+GPIO_Error_t GPIO_GetPinValue(GPIO_PORT_t GPIO_PORT,GPIO_PIN_t GPIO_PIN,uint8 * Copy_PinValue);
