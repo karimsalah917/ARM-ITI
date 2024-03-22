f354469abe565eb56f863d8c346af0b7b253ce49
Karim Salah <karimsalah917@gmail.com>
2024-03-22 17:48:06 +0200

    Documentation

--- BLACKPILL/src/4-Service/SCHED_Config.c
+++ BLACKPILL/src/4-Service/SCHED_Config.c
@@ -13,13 +13,28 @@
 #include "../../include/4-Service/SCHED_Config.h"
 /********************************** Definitions ********************************************/
 /**********************************   Types ************************************************/
-const RunnableTask_t RUN_LIST[_Runnable_no]=
-{
-    [Run1]=
-    {
-        .TaskName="LED State Machine",
-        .PeriodicityMS=100,
-        .InitialDelayMS=0,
-        .CallBack=TrafficLight_Runnable
+
+/**
+ * Runnable task list configuration.
+ * 
+ * This constant array defines the list of tasks to be executed by the scheduler.
+ * Each task is represented by a RunnableTask_t structure containing details such as task name,
+ * periodicity, initial delay, and callback function.
+ * 
+ * Task Details:
+ * - TaskName: "LED State Machine" - Descriptive name of the task.
+ * - PeriodicityMS: 100 - Task execution frequency in milliseconds.
+ * - InitialDelayMS: 0 - Initial delay before the task starts execution.
+ * - CallBack: TrafficLight_Runnable - Callback function associated with the task.
+ * 
+ * The example provided initializes a single task to run the TrafficLight_Runnable function,
+ * which controls the traffic light state machine.
+ */
+const RunnableTask_t RUN_LIST[_Runnable_no] = {
+    [Run1] = {
+        .TaskName = "LED State Machine",
+        .PeriodicityMS = 100,
+        .InitialDelayMS = 0,
+        .CallBack = TrafficLight_Runnable
     }
 };
